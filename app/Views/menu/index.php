3dfcff88a8c0e745e3b2dab71b68d8b782f650a5
Montalvo566 <angelmontalvo854@gmail.com>
2024-08-02 16:43:13 -0600

    Version con Codenigther
    Version con Codenigther

---
+++ app/Views/menu/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width, shrink-to-fit=no">
+  <title>Menu principal</title>
+
+  <link rel="icon" href="./Documents/Icons/tourVirtualcon.png" type="image/x-icon">
+  <link rel="stylesheet" href="<?= base_url('resources/libs/fontawsome/css/all.min.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('resources/css/menu.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('resources/libs/sweetalert/sweetAlert.min.css') ?>">
+  <link href="https://fonts.googleapis.com/css?family=Julius+Sans+One|Press+Start+2P" rel="stylesheet">
+</head>
+<body>
+
+  <!-- Contenido principal -->
+  <div id="main-content">
+    <!-- Boton del comenzar -->
+    <div id="start-button" class="btn-arcade">Comenzar recorrido</div>
+
+    <!-- Dialogo typeado -->
+    <div id="dialog">
+      <div id="typed"></div>
+    </div>
+
+    <!-- Imagenes de los avatar -->
+    <div id="gender-selection" style="display: none;">
+      <div class="avatar male" onmouseover="showGender('masculino')" onmouseout="hideGender()" onclick="selectGender('masculino')">
+        <img src="<?= base_url('resources/img/gifs/AvatarMasculino.gif')?>">
+        <div class="gender-label" id="masculino-label">Masculino</div>
+      </div>
+      <div class="avatar female" onmouseover="showGender('femenino')" onmouseout="hideGender()" onclick="selectGender('femenino')">
+        <img src="<?= base_url('resources/img/gifs/AvatarFemenino.gif') ?>" alt="Avatar Femenino">
+        <div class="gender-label" id="femenino-label">Femenino</div>
+      </div>
+    </div>
+    <!-- Fin -->
+
+    <!-- Botón para guardar el género -->
+    <div id="save-gender-button" style="display: none;" class="btn-generoLS" onclick="saveGender()">Guardar Género</div>
+
+    <!-- Fin -->
+
+  </div>
+  <!-- Fin -->
+
+  <script src="<?= base_url('resources/libs/typed/typed.min.js') ?>"></script>
+  <script src="<?= base_url('resources/js/menu/menu.js') ?>"></script>
+  <script src="<?= base_url('resources/libs/sweetalert/sweetAlert.min.js') ?>"></script>
+  <script>
+    var baseUrl = "<?= base_url() ?>";
+  </script>
+  <script src="./Lib/easterEgg.js"></script>
+</body>
+</html>
---
+++ resources/js/menu/menu.js
@@ -0,0 +1,118 @@
+// Variable global para manejar la instancia de Typed.js
+var typedInstance = null;
+var selectedGender = null;
+
+document.getElementById('start-button').addEventListener('click', function () {
+  document.getElementById('start-button').style.display = 'none';
+  document.getElementById('typed').style.display = 'block';
+  startAnimation();
+});
+
+function startAnimation() {
+  // Asegúrate de que no haya una instancia previa de Typed.js
+  if (typedInstance) {
+    typedInstance.destroy();
+  }
+
+  typedInstance = new Typed("#typed", {
+    strings: [
+      '<strong>¡Bienvenido!</strong>',
+      '<strong>Antes de comenzar, seleccione su género</strong>'
+    ],
+    typeSpeed: 50,
+    showCursor: false,
+    startDelay: 500,
+    onComplete: function() {
+      setTimeout(function() {
+        document.getElementById('typed').style.display = 'none';
+        document.getElementById('gender-selection').style.display = 'flex';
+      }, 1000);
+    }
+  });
+}
+
+// Función para seleccionar el género
+function selectGender(gender) {
+  // Almacenar el género seleccionado en una variable global
+  selectedGender = gender;
+
+  // Mostrar el botón de guardar
+  document.getElementById('save-gender-button').style.display = 'block';
+
+  var welcomeText = gender === 'masculino'
+    ? '<strong>Caballero</strong>'
+    : '<strong>Dama</strong>';
+
+  // Asegúrate de que no haya una instancia previa de Typed.js
+  if (typedInstance) {
+    typedInstance.destroy();
+  }
+
+  // Mostrar el mensaje de bienvenida
+  typedInstance = new Typed("#typed", {
+    strings: [
+      welcomeText
+    ],
+    typeSpeed: 50,
+    showCursor: false,
+    startDelay: 500,
+  });
+}
+
+// Función para guardar el género en el localStorage
+function saveGender() {
+  if (selectedGender) {
+    localStorage.setItem('selectedGender', selectedGender);
+    console.log(selectedGender);
+    
+    document.getElementById('save-gender-button').style.display = 'none';
+    document.getElementById('gender-selection').style.display = 'none';
+    continueAnimation();
+  } else {
+    console.error('No se ha seleccionado ningún género.');
+  }
+}
+
+function continueAnimation() {
+  // Asegúrate de que no haya una instancia previa de Typed.js
+  if (typedInstance) {
+    typedInstance.destroy();
+  }
+
+  // Asegúrate de que el elemento #typed esté visible
+  document.getElementById('typed').style.display = 'block';
+
+  typedInstance = new Typed("#typed", {
+    strings: [
+      '<strong>Vive REDESLA presenta</strong>',
+      'Un recorrido virtual por nuestras instalaciones',
+      '¿Estás listo?',
+      '¡Disfruta de tu experiencia inmersiva!'
+    ],
+    typeSpeed: 50,
+    showCursor: false,
+    startDelay: 500,
+    onComplete: function() {
+      setTimeout(function() {
+        document.body.style.opacity = 0;
+        setTimeout(function() {
+          window.location.href = baseUrl + 'views/inicio';
+        }, 400);
+      }, 1800);
+    }
+  });
+}
+
+// Funciones para mostrar y ocultar la etiqueta de género
+function showGender(gender) {
+  var label = document.getElementById(gender + '-label');
+  if (label) {
+    label.style.display = 'block';
+  }
+}
+
+function hideGender() {
+  document.querySelectorAll('.gender-label').forEach(function(label) {
+    label.style.display = 'none';
+  });
+}
